97ae98a7d2b5b2cd8a54a2f7a3a5bc68c418a85d
Burak Kal <burak0054@gmail.com>
2017-10-03 23:33:04 +0300

    Initial commit

---
+++ app/src/main/java/com/burakkal/tmdb/TvShowsActivity.java
@@ -0,0 +1,13 @@
+package com.burakkal.tmdb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TvShowsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tv_shows);
+    }
+}
